d22e2c152e857245c7aefd0c79aff611ae0be7eb
carokitu <44806926+carokitu@users.noreply.github.com>
2025-07-23 12:00:43 +0200

    72-change-like-overlay-and-button (#81)
    Co-authored-by: Caroline de Moulins <caroline.demoulins@yahoo.com>

--- src/components/Post/InteractiveContainer.tsx
+++ src/components/Post/InteractiveContainer.tsx
@@ -1,59 +1,73 @@
-import React, { useRef, useState } from 'react'
+import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react'
 import { Animated, Pressable, type StyleProp, StyleSheet, View, type ViewStyle } from 'react-native'
 
 import { useDoubleTap } from '../../../hooks'
 import { theme } from '../../theme'
 
-export const InteractiveContainer = ({
-  children,
-  handleLike,
-  style,
-}: {
-  children: React.ReactNode
-  handleLike: () => void
-  style?: StyleProp<ViewStyle>
-}) => {
+export type InteractiveContainerRef = {
+  triggerOverlay: () => void
+}
+
+export const InteractiveContainer = forwardRef<
+  InteractiveContainerRef,
+  {
+    children: React.ReactNode
+    handleLike: () => void
+    style?: StyleProp<ViewStyle>
+  }
+>(({ children, handleLike, style }, ref) => {
   const [showLikeOverlay, setShowLikeOverlay] = useState(false)
   const scale = useRef(new Animated.Value(0)).current
 
-  const likePost = () => {
+  const triggerOverlay = () => {
     setShowLikeOverlay(true)
     Animated.sequence([
       Animated.timing(scale, { duration: 150, toValue: 1, useNativeDriver: true }),
       Animated.timing(scale, { delay: 300, duration: 300, toValue: 0, useNativeDriver: true }),
     ]).start(() => setShowLikeOverlay(false))
+  }
+
+  const likePost = () => {
+    triggerOverlay()
     handleLike()
   }
 
   const handleTap = useDoubleTap({ onDoubleTap: () => likePost() })
+
+  useImperativeHandle(ref, () => ({
+    triggerOverlay,
+  }))
 
   return (
     <Pressable onPress={handleTap}>
       <View style={[styles.post, style]}>
         {children}
         {showLikeOverlay && (
-          <Animated.Image
-            // eslint-disable-next-line @typescript-eslint/no-require-imports
-            source={require('../../../assets/images/post-overlay/like.png')}
-            style={[
-              styles.overlay,
-              {
+          <View style={styles.overlay}>
+            <Animated.Image
+              // eslint-disable-next-line @typescript-eslint/no-require-imports
+              source={require('../../../assets/images/icons/heart-filled.png')}
+              style={{
                 opacity: scale,
                 transform: [{ scale }],
-              },
-            ]}
-          />
+              }}
+            />
+          </View>
         )}
       </View>
     </Pressable>
   )
-}
+})
 
 const styles = StyleSheet.create({
   overlay: {
-    height: '100%',
+    alignItems: 'center',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
     position: 'absolute',
-    width: '100%',
+    right: 0,
+    top: 0,
   },
   post: {
     backgroundColor: theme.surface.brand.default,
